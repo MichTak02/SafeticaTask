b06d59b5c4721c0a7387296846c15f27348a4048
Michal Takac <xtakac@fi.muni.cz>
2025-01-24 15:27:31 +0100

    feat: Added additional methods in TestLogger for logging test start and test result

--- SafeticaTask/TestLogger.cs
+++ SafeticaTask/TestLogger.cs
@@ -17,4 +17,33 @@ public class TestLogger(string logFilePath)
         using StreamWriter writer = new StreamWriter(LogFilePath, append: true);
         writer.WriteLine($"{DateTime.Now:s} {message}");
     }
+
+    
+    /// <summary>
+    /// Logs start of test
+    /// </summary>
+    /// <param name="testName">name of current test</param>
+    public void LogTestStart(string testName)
+    {
+        LogAction($"Starting test: {testName}");
+    }
+
+    /// <summary>
+    /// Logs successful test
+    /// </summary>
+    /// <param name="testName">Name of current test</param>
+    public void LogTestPassed(string testName)
+    {
+        LogAction($"Test {testName} passed");
+    }
+
+    /// <summary>
+    /// Logs failed test
+    /// </summary>
+    /// <param name="testName">Name of current test</param>
+    /// <param name="failMessage">Fail message</param>
+    public void LogTestFailed(string testName, string failMessage)
+    {
+        LogAction($"Test {testName} failed: {failMessage}");
+    }
 }

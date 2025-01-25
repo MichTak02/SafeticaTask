a8de1ab9f74231f1a4a98985d8dc481ce84e162a
Michal Takac <xtakac@fi.muni.cz>
2025-01-25 20:03:02 +0100

    feat: logging start and end of test in CommonTest

--- SafeticaTask/CommonTest.cs
+++ SafeticaTask/CommonTest.cs
@@ -1,15 +1,30 @@
+using NUnit.Framework.Interfaces;
+
 namespace SafeticaTask;
 
 public class CommonTest
 {
+    private readonly TestLogger _testLogger = new(TestLogger.DefaultLogFilePath);
+    private string TestName => TestContext.CurrentContext.Test.FullName;
+
     [SetUp]
     public void Setup()
     {
+        _testLogger.LogTestStart(TestName);
     }
 
-    [Test]
-    public void Test1()
+    [TearDown]
+    public void TearDown()
     {
-        Assert.Pass();
+        var testResult = TestContext.CurrentContext.Result.Outcome;
+
+        if (testResult.Equals(ResultState.Success))
+        {
+            _testLogger.LogTestPassed(TestName);
+            return;
+        }
+
+        var failMessage = TestContext.CurrentContext.Result.Message ?? "";
+        _testLogger.LogTestFailed(TestName, failMessage);
     }
 }

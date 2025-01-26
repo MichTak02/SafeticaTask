bc9763dd5dbc7ee433f38c3591b311b92f0ea91c
Michal Takac <xtakac@fi.muni.cz>
2025-01-26 17:59:17 +0100

    feat(TeamsActions): Implemented login

--- SafeticaTask/TeamsActions.cs
+++ SafeticaTask/TeamsActions.cs
@@ -3,12 +3,69 @@ using OpenQA.Selenium.Support.UI;
 
 namespace SafeticaTask;
 
-public class TeamsActions(WebDriver webDriver, TestLogger logger)
+public class TeamsActions
 {
-    public static readonly string TeamsUrl = "https://teams.microsoft.com/v2/";
-    public static readonly string DefaultLogin = "qa@safeticaservices.onmicrosoft.com";
-    public static readonly string DefaultPassword = "automation.Safetica2004";
+    private static readonly string TeamsUrl = "https://teams.microsoft.com/v2/";
+    private static readonly string DefaultLogin = "qa@safeticaservices.onmicrosoft.com";
+    private static readonly string DefaultPassword = "automation.Safetica2004";
+
+    private static readonly string LoginFieldId = "i0116";
+    private static readonly string PasswordFieldId = "i0118";
+    private static readonly string SubmitButtonId = "idSIButton9";
     
-    public WebDriver WebDriver { get; } = webDriver;
-    public TestLogger Logger { get; } = logger;
+
+    public WebDriver WebDriver { get; }
+    public TestLogger Logger { get; }
+    public WebDriverWait Wait { get; }
+    
+    public TeamsActions(WebDriver webDriver, TestLogger logger)
+    {
+        WebDriver = webDriver;
+        Logger = logger;
+        Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(10));
+    }
+    
+    
+    public void LogIn(string login, string password)
+    {
+        WebDriver.Navigate().GoToUrl(TeamsUrl);
+        
+        FillField(LoginFieldId, login);
+        ClickButton(SubmitButtonId);
+        
+        FillField(PasswordFieldId, password);
+        string url = WebDriver.Url;
+        ClickButton(SubmitButtonId);
+        Wait.Until(_ => url != WebDriver.Url);
+        
+        ClickButton(SubmitButtonId);
+    }
+
+    public void LogIn()
+    {
+        LogIn(DefaultLogin, DefaultPassword);
+    }
+
+    private void FillField(string id, string text)
+    {
+        IWebElement element = WaitForDisplayed(id);
+        
+        element.Clear();
+        element.SendKeys(text);
+    }
+
+    private void ClickButton(string id)
+    {
+        IWebElement button = WaitForDisplayed(id);
+        button.Click();
+    }
+
+    private IWebElement WaitForDisplayed(string id)
+    {
+        return Wait.Until(driver =>
+        {
+            var element = driver.FindElement(By.Id(id));
+            return element.Displayed ? element : null;
+        });
+    }
 }

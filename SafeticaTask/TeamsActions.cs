414976f523e8bb453ded9695811bc6e19ef91d14
Michal Takac <xtakac@fi.muni.cz>
2025-01-26 20:24:48 +0100

    feat(TeamsActions): implemented selecting chat

--- SafeticaTask/TeamsActions.cs
+++ SafeticaTask/TeamsActions.cs
@@ -12,36 +12,38 @@ public class TeamsActions
     private static readonly string LoginFieldId = "i0116";
     private static readonly string PasswordFieldId = "i0118";
     private static readonly string SubmitButtonId = "idSIButton9";
-    
+    private static readonly string DefaultChatName = "Safetica QA";
+
 
     public WebDriver WebDriver { get; }
     public TestLogger Logger { get; }
     public WebDriverWait Wait { get; }
-    
+
     public TeamsActions(WebDriver webDriver, TestLogger logger)
     {
         WebDriver = webDriver;
         Logger = logger;
         Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(10));
     }
-    
-    
+
+
     public void LogIn(string login, string password)
     {
         Logger.LogAction($"Navigating to {TeamsUrl}");
         WebDriver.Navigate().GoToUrl(TeamsUrl);
-        
+
         Logger.LogAction("Filling in a login and going to next page");
         FillField(LoginFieldId, login);
         ClickButton(SubmitButtonId);
-        
+
         Logger.LogAction("Filling password and going to next page");
         FillField(PasswordFieldId, password);
         string url = WebDriver.Url;
         ClickButton(SubmitButtonId);
-        
+
         Logger.LogAction("Confirming to stay logged in");
-        Wait.Until(_ => url != WebDriver.Url);  // Wait for "stay logged in?" screen before clicking button with the same id
+        Wait.Until(_ =>
+            url != WebDriver.Url); // Wait for "stay logged in?" screen before clicking button with the same id
         ClickButton(SubmitButtonId);
     }
 
@@ -50,10 +52,30 @@ public class TeamsActions
         LogIn(DefaultLogin, DefaultPassword);
     }
 
+    public void SelectChat(string chatName)
+    {
+        Logger.LogAction($"Selecting chat '{chatName}'");
+        string chatXPath = $"//*[starts-with(@title, '{chatName}')]";
+
+        Wait.Until(driver =>
+        {
+            var element = driver.FindElement(By.XPath(chatXPath));
+            return element.Displayed ? element : null;
+        });
+
+        WebDriver.FindElement(By.XPath(chatXPath)).Click();
+        Thread.Sleep(5000);
+    }
+
+    public void SelectChat()
+    {
+        SelectChat(DefaultChatName);
+    }
+
     private void FillField(string id, string text)
     {
         IWebElement element = WaitForDisplayed(id);
-        
+
         element.Clear();
         element.SendKeys(text);
     }

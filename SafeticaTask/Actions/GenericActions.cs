037cfb65642966d5b48a5292e044bccf44382201
Michal Takac <xtakac@fi.muni.cz>
2025-01-28 00:28:39 +0100

    docs(GenericActions): added documentation

--- SafeticaTask/Actions/GenericActions.cs
+++ SafeticaTask/Actions/GenericActions.cs
@@ -4,6 +4,9 @@ using OpenQA.Selenium.Support.UI;
 
 namespace SafeticaTask.Actions;
 
+/// <summary>
+/// Class for utility methods to perform common actions on web pages using Selenium
+/// </summary>
 public class GenericActions
 {
     public WebDriverWait Wait { get; }
@@ -13,18 +16,33 @@ public class GenericActions
         Wait = wait;
     }
     
+    /// <summary>
+    /// Waits for input field to load and fills in a text
+    /// </summary>
+    /// <param name="by">input field identifier</param>
+    /// <param name="text">text to input</param>
     public void FillField(By by, string text)
     {
         IWebElement element = WaitForDisplayed(by);
         element.SendKeys(text);
     }
     
+    /// <summary>
+    /// Waits for button / clickable element to load and clicks it
+    /// </summary>
+    /// <param name="by">button identifier</param>
+    /// <param name="mustBeDisplayed">true if button must be displayed to click it</param>
     public void ClickButton(By by, bool mustBeDisplayed = true)
     {
         IWebElement button = mustBeDisplayed ? WaitForDisplayed(by) : Wait.Until(driver => driver.FindElement(by));
         button.Click();
     }
     
+    /// <summary>
+    /// Waits for element to load and be displayed
+    /// </summary>
+    /// <param name="by">element identifier</param>
+    /// <returns></returns>
     public IWebElement WaitForDisplayed(By by)
     {
         return Wait.Until(driver =>
@@ -34,13 +52,31 @@ public class GenericActions
         });
     }
 
+    /// <summary>
+    /// Waits for exact number of elements
+    /// </summary>
+    /// <param name="by">elements identifier</param>
+    /// <param name="number">number of elements to get</param>
+    /// <returns></returns>
     public ReadOnlyCollection<IWebElement> GetMultipleElements(By by, int number)
     {
         return GetMultipleElementsRange(by, number, number);
     }
 
+    /// <summary>
+    /// Waits for elements in range
+    /// </summary>
+    /// <param name="by">elements identifier</param>
+    /// <param name="min">minimum elements to load</param>
+    /// <param name="max">maximum elements to load or -1 for no upper bound</param>
+    /// <returns></returns>
     public ReadOnlyCollection<IWebElement> GetMultipleElementsRange(By by, int min, int max)
     {
+        if (max == -1)
+        {
+            max = int.MaxValue;
+        }
+        
         return Wait.Until(driver =>
         {
             var elements = driver.FindElements(by);

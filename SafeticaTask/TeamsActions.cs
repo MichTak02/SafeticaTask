d97954b5bd3181766fdd27c63b08047a54138a47
Michal Takac <xtakac@fi.muni.cz>
2025-01-27 16:12:11 +0100

    feat(TeamsActions): implemented attaching file from OneDrive

--- SafeticaTask/TeamsActions.cs
+++ SafeticaTask/TeamsActions.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
@@ -13,7 +14,15 @@ public class TeamsActions
     private static readonly string PasswordFieldId = "i0118";
     private static readonly string SubmitButtonId = "idSIButton9";
     private static readonly string DefaultChatName = "Safetica QA";
+    private static readonly string DeafultFileName = "PdfFile.pdf";
 
+    private static readonly string PlusSymbolName = "message-extension-flyout-command";
+    private static readonly string FlyoutListDataTid = "flyout-list-item";
+    private static readonly string AttachFromCloudDataTid = "file-attach-from-onedrive";
+    private static readonly string FileSelectPopupAttribute = "aria-label";
+    private static readonly string MyFilesClassName = "navLink_26dbef85";
+    private static readonly string PrimaryButtonClassName = "ms-Button--primary";
+    
 
     public WebDriver WebDriver { get; }
     public TestLogger Logger { get; }
@@ -23,7 +32,7 @@ public class TeamsActions
     {
         WebDriver = webDriver;
         Logger = logger;
-        Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(10));
+        Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(15));
     }
 
 
@@ -67,6 +76,42 @@ public class TeamsActions
     {
         SelectChat(DefaultChatName);
     }
+    
+    public void AttachFile(string fileName)
+    {
+        // Click plus symbol
+        WaitForDisplayed(By.Name(PlusSymbolName)).Click();
+        
+        // Select attach file
+        var flyoutListItems = GetMultipleElements(ByDataTid(FlyoutListDataTid), 3);
+        
+        var attachFileId = flyoutListItems.Select(item => item.GetAttribute("id")).First();
+        ClickButton(attachFileId);
+
+        // Select attach cloud file
+        WaitForDisplayed(ByDataTid(AttachFromCloudDataTid)).Click();
+
+        // Switch to popup window
+        var iframes = GetMultipleElements(By.TagName("iframe"), 2);
+        var iframe = iframes.First(frame => frame.GetAttribute(FileSelectPopupAttribute) is not null);
+        WebDriver.SwitchTo().Frame(iframe);
+
+        // Select My files
+        Wait.Until(driver => driver.FindElement(By.ClassName(MyFilesClassName))).Click();
+        
+        // Select File
+        WaitForDisplayed(By.XPath($"//*[text()='{fileName}']")).Click();
+
+        // Click on attach file
+        WaitForDisplayed(By.ClassName(PrimaryButtonClassName)).Click();
+        
+        WebDriver.SwitchTo().DefaultContent();
+    }
+
+    public void AttachFile()
+    {
+        AttachFile(DeafultFileName);
+    }
 
     private void FillField(string id, string text)
     {
@@ -94,5 +139,19 @@ public class TeamsActions
             var element = driver.FindElement(by);
             return element.Displayed ? element : null;
         });
+    }
+
+    private ReadOnlyCollection<IWebElement> GetMultipleElements(By by, int number)
+    {
+        return Wait.Until(driver =>
+        {
+            var elements = driver.FindElements(by);
+            return elements.Count == number ? elements : null;
+        });
+    }
+
+    private By ByDataTid(string value)
+    {
+        return By.XPath($"//*[@data-tid='{value}']");
     }
 }

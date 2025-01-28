66c835c625be5a321c0f07bf0c5dab6f385badf6
Michal Takac <xtakac@fi.muni.cz>
2025-01-28 20:04:52 +0100

    test(TeamsChromeTest, TeamsFirefoxTest): added browser-specific setups

--- SafeticaTask/TeamsChromeTest.cs
+++ SafeticaTask/TeamsChromeTest.cs
@@ -1,6 +1,20 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SafeticaTask.Actions;
+
 namespace SafeticaTask;
 
 public class TeamsChromeTest
 {
     
+public class TeamsChromeTest : CommonTest
+{
+    [SetUp]
+    public void ChromeSetup()
+    {
+        Driver = new ChromeDriver();
+        Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+        GenericActions = new GenericActions(Wait);
+        TeamsActions = new TeamsActions(Driver, TestLogger, Wait, GenericActions);
+    }
 }
--- SafeticaTask/TeamsFirefoxTest.cs
+++ SafeticaTask/TeamsFirefoxTest.cs
@@ -1,6 +1,19 @@
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using SafeticaTask.Actions;
+
 namespace SafeticaTask;
 
-public class TeamsFirefoxTest
+public class TeamsFirefoxTest : CommonTest
 {
-    
+    [SetUp]
+    public void FirefoxSetup()
+    {
+        var options = new FirefoxOptions();
+        options.SetPreference("network.cookie.cookieBehavior", 0);
+        Driver = new FirefoxDriver(options);
+        Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+        GenericActions = new GenericActions(Wait);
+        TeamsActions = new TeamsActions(Driver, TestLogger, Wait, GenericActions);
+    }
 }

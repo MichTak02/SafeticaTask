b77fdae62ca69bafca5bdf4f6dcfc14abb55d714
Michal Takac <xtakac@fi.muni.cz>
2025-01-28 20:17:55 +0100

    test(TeamsChromeTest): added test for sending one text message

--- SafeticaTask/TeamsChromeTest.cs
+++ SafeticaTask/TeamsChromeTest.cs
@@ -30,4 +30,19 @@ public class TeamsChromeTest : CommonTest
         Assert.That(message?.Files.Count, Is.EqualTo(1));
         Assert.That(message?.Files[0], Is.EqualTo(filename));
     }
+    
+    [Test]
+    public void SendSingleMessage()
+    {
+        string messageText = $"{TestName}.{TestContext.CurrentContext.Test.ID}";
+        TeamsActions?.LogIn();
+        TeamsActions?.SelectChat();
+        DateTime beginSendTime = DateTime.Now;
+        TeamsActions?.SendMessage(messageText);
+        var message = TeamsActions?.GetLastMessages(1)[0];
+        
+        Assert.That(message?.TimeSent, Is.GreaterThanOrEqualTo(beginSendTime));
+        Assert.That(message?.Files.Count, Is.EqualTo(0));
+        Assert.That(message?.Message, Is.EqualTo(messageText));
+    }
 }

f00025ccbd27792308c569be5bf6286da2c8343f
Michal Takac <xtakac@fi.muni.cz>
2025-01-28 21:04:36 +0100

    docs(ByExtensions): added documentation

--- SafeticaTask/Utils/ByExtensions.cs
+++ SafeticaTask/Utils/ByExtensions.cs
@@ -2,13 +2,26 @@ using OpenQA.Selenium;
 
 namespace SafeticaTask.Utils;
 
+/// <summary>
+/// Class to extend By functionality
+/// </summary>
 public static class ByExtensions
 {
+    /// <summary>
+    /// Allows element to by selected by data-tid
+    /// </summary>
+    /// <param name="value">data-tid value</param>
+    /// <returns>By instance allowing to search data-tid</returns>
     public static By ByDataTid(string value)
     {
         return By.XPath($"//*[@data-tid='{value}']");
     }
 
+    /// <summary>
+    /// Allows element to by selected by data-testid
+    /// </summary>
+    /// <param name="value">data-testid value</param>
+    /// <returns>By instance allowing to search data-testid</returns>
     public static By ByDataTestId(string value)
     {
         return By.XPath($"//*[@data-testid='{value}']");

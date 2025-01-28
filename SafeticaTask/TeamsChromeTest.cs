065546d1675fe46eb9cbc467fea0bbb7b7d90ccf
Michal Takac <xtakac@fi.muni.cz>
2025-01-28 20:07:42 +0100

    test(TeamsChromeTest): added test for sending one OneDrive file

--- SafeticaTask/TeamsChromeTest.cs
+++ SafeticaTask/TeamsChromeTest.cs
@@ -4,9 +4,6 @@ using SafeticaTask.Actions;
 
 namespace SafeticaTask;
 
-public class TeamsChromeTest
-{
-    
 public class TeamsChromeTest : CommonTest
 {
     [SetUp]
@@ -16,5 +13,21 @@ public class TeamsChromeTest : CommonTest
         Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
         GenericActions = new GenericActions(Wait);
         TeamsActions = new TeamsActions(Driver, TestLogger, Wait, GenericActions);
+    }
+
+    [Test]
+    public void SendSingleOneDriveFile()
+    {
+        string filename = TeamsActions.DefaultFileName1;
+        TeamsActions?.LogIn();
+        TeamsActions?.SelectChat();
+        DateTime beginSendTime = DateTime.Now;
+        TeamsActions?.SendFile(filename);
+        DateTime afterSendTime = DateTime.Now;
+        var message = TeamsActions?.GetLastMessages(1)[0];
+        
+        Assert.That(message?.TimeSent, Is.GreaterThanOrEqualTo(beginSendTime).And.LessThanOrEqualTo(afterSendTime));
+        Assert.That(message?.Files.Count, Is.EqualTo(1));
+        Assert.That(message?.Files[0], Is.EqualTo(filename));
     }
 }
